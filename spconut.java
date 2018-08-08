99391346d7b2564d98d3e78074e3516042811f34
keerthichintala <41735356+keerthichintala@users.noreply.github.com>
2018-08-08 22:53:40 +0530

    Create spconut.java

---
+++ spconut.java
@@ -0,0 +1,27 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		String str=s.nextLine();
+		int len=str.length();
+		int k=0;
+		for(int i=0;i<len;i++)
+		{
+		if((str.charAt(i)>='a'&&str.charAt(i)<='z')||(str.charAt(i)>='A'&&str.charAt(i)<='Z')||(str.charAt(i)>=48&&str.charAt(i)<=57)||str.charAt(i)==' ')
+	continue;
+		else
+		k++;
+		}
+	System.out.println(k);	
+
+	}
+}
